2f3dbed51c88af01a4e2e3cf0c13aeacdde023fb
Patrik Pukán <patrik.pukan@gmail.com>
2023-08-19 01:17:03 +0200

    change calc logic

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,24 @@ import Field from './Field.jsx';
 
 
 function App() {
-  const [currDisplay, setCurrDisplay] = useState('test text')
+  const [currDisplay, setCurrDisplay] = useState('Enter numbers')
   const plus = '+'
   const numbers = [0,1,2,3,4,5,6,7,8,9,3]
-  const handleClick = (event) => {
+  const handleNumbers = (event) => {
     setCurrDisplay(event.target.textContent);
     // console.log(event)
     // console.log(event.target)
+  }
+  const handleClick = (event) => {
+
   }
 
   return (
     <>
       <Field currentDisplay={currDisplay}></Field>
       <PlusB onClick={handleClick} textValue='+'></PlusB>
-      <NumbersGrid onClick={handleClick}></NumbersGrid>
+      <clearB onClick={handleClick}></clearB>
+      <NumbersGrid onClick={handleNumbers}></NumbersGrid>
     </>
     )
   }
